32fd7eea0e33ca9b30e3d4505198c93537f615b5
Tejaswi Nallavolu <tejaswireddynallavolu@gmail.com>
2021-10-14 01:45:15 -0500

    ClassReminderApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//import { ModalModule } from 'ngx-bootstrap';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './user/header/header.component';
+import { LoginComponent } from './user/login/login.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //ModalModule.forRoot(),
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+/*
+      {path: 'home', component: },
+*/
+    ]),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
